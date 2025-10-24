5ff97f58d846360ab344a596bedb963abeb8b13d
Kenn Xavier <kennxavier15@gmail.com>
2025-10-24 14:36:31 +0800

    added temporary logo

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -2,17 +2,24 @@ import { Link } from 'react-router-dom'
 import Thumbnail from './Thumbnail'
 import characterImage from '../assets/character.png'
 import './LandingPage.css'
+import logo from '../assets/logo.png'
+import logoSvg from '../assets/logo-dark.png'
 
 const LandingPage = () => {
   return (
     <div className="landing-page">
       <header className="header">
-        <div className="logo">logo</div>
+        <div className="logo">
+          <img src={logo} alt="Hygin Logo" className="logo-image" />
+        </div>
       </header>
       
       <main className="main-content">
         <div className="left-section">
-          <h1 className="title">Hygin</h1>
+          {/* <h1 className="title">Hygin</h1> */}
+          <div className="svg-logo">
+            <img src={logoSvg} alt="Hygin logo" className="title-logo" />
+          </div>
           <p className="description">
             Hygin is an engaging, web-based gamification platform designed to teach essential hygiene practices to children in Grades 3-6. The game transforms daily routines into a fun, interactive experience by using game elements like points, levels, and challenges. Players learn about and are motivated to practice key hygiene habits, specifically handwashing, tooth brushing, and hair washing, with the goal of building lifelong healthy behaviors.
           </p>
